8c515c7c2389c170896c3f5c027393d8af9a5d1e
Shafiur Rahman <128807435+shafiur2409@users.noreply.github.com>
2024-01-06 20:14:42 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+      title: Text('Jargon Lens'),
+      centerTitle: true,
+      backgroundColor: Colors.blue,
+      ),
+   body:Center(
+      child: Image(
+        image: NetworkImage('https://cdn-icons-png.flaticon.com/128/3898/3898082.png'),
+    
+    
+      
+      ),
+      ),
+        ),
+    ));
+}
