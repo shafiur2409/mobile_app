0781760894a2cc18bcf7ea5f013a70eb411277f6
Ramyaa S <johndoe@example.com>
2024-01-06 21:56:35 +0530

    signin commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,48 @@ void main() {
         ),
     ));
 }
+Text(
+    'Sign in',
+    style: TextStyle(
+        color: Colors.black,
+        fontSize: 15,
+        fontFamily: 'Times New Roman',
+        fontWeight: FontWeight.w700,
+        height: 0,
+    ),
+)
+Container(
+    width: 276,
+    decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+            side: BorderSide(
+                width: 1,
+                strokeAlign: BorderSide.strokeAlignCenter,
+            ),
+        ),
+    ),
+)
+Container(
+    width: 20,
+    height: 20,
+    decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage("https://via.placeholder.com/20x20"),
+            fit: BoxFit.fill,
+        ),
+    ),
+)
+SizedBox(
+    width: 51,
+    height: 13,
+    child: Text(
+        'username\n',
+        style: TextStyle(
+            color: Colors.black,
+            fontSize: 10,
+            fontFamily: 'Times New Roman',
+            fontWeight: FontWeight.w400,
+            height: 0,
+        ),
+    ),
+)
